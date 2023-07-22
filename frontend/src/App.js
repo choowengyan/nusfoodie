a504cc9ff3ed84e8072b2cdd445239fd9eb82c39
wengyan <wengstudies0407@gmail.com>
2023-07-22 18:13:04 +0800

    remove unwanted links from app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,8 @@
-
-import './App.css';
-import Navbar from './containers/navbar';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import { Switch } from '@material-ui/core';
-import { createMuiTheme } from '@material-ui/core/styles';
 import Establishments from './containers/Establishments/Establishments';
 import { createTheme, ThemeProvider } from '@mui/material';
+
+import './App.css';
 import Cuisines from './containers/Cuisines/Cuisines';
 import Home from './containers/Home/Home';
 import Stalls from './containers/Stalls/Stalls';
@@ -25,43 +22,29 @@ const theme = createTheme({
 });
 
 function App() {
-  return (
-    <ThemeProvider theme={theme}>
-      <div className="App">
-        {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-        <Router>
-          {/* <Navbar /> */}
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<SignUp />} />
-            <Route path="/establishments" element={<Establishments />} />
-            <Route path="/establishments/:id" element={<Stalls />} />
-            <Route path="/establishments/:id/:stallId" element={<StallItems />} />
-            <Route path="/cuisines" element={<Cuisines />} />
-            <Route path="/reviews" element={<Reviews />} />
-            <Route path="/review-form" element={<ReviewForm />} />
-          </Routes>
-        </Router>
-        {/* <h1>
-        Hello React test
-      </h1> */}
-      </div>
-    </ThemeProvider>
+  const [cookies] = useCookies(['user']);
+  const [user, setUser] = useState(cookies.user || null);
 
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      <ThemeProvider theme={theme}>
+        <div className="App">
+          <Router>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<SignUp />} />
+              <Route path="/establishments" element={<Establishments />} />
+              <Route path="/establishments/:id" element={<Stalls />} />
+              <Route path="/establishments/:id/:stallId" element={<StallItems />} />
+              <Route path="/cuisines" element={<Cuisines />} />
+              <Route path="/reviews" element={<Reviews />} />
+              <Route path="/review-form" element={<ReviewForm />} />
+            </Routes>
+          </Router>
+        </div>
+      </ThemeProvider>
+    </UserContext.Provider>
   );
 }
 
