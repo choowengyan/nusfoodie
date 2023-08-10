0c27673251823d124511c8e48f2b0031e4269722
wengyan <64072147+choowengyan@users.noreply.github.com>
2023-08-11 00:07:43 +0800

    update on axios for data loading

--- frontend/src/containers/Stalls/Stalls.js
+++ frontend/src/containers/Stalls/Stalls.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import React, { useEffect, useState } from 'react';
+import { Container } from 'react-bootstrap';
 import axios from "axios";
-import ResponsiveAppBar from '../navbar';
 
+import ResponsiveAppBar from '../navbar';
 import './Stalls.css';
 import StallListings from '../components/cards/StallListings';
 
@@ -14,187 +14,62 @@ const styles = {
     }
 };
 
-class Stalls extends Component {
-    constructor(props) {
-        super(props);
-    }
+const url = 'http://34.126.91.148:5001/api'
 
-    // componentDidMount() {
-    //     this.fetchCourses();
-    // }
+const Stalls = () => {
+    const [stalls, setStalls] = useState([]);
+    const [establishmentName, setEstablishmentName] = useState('');
 
-    // componentDidUpdate(prevProps, prevState) {
-    //     if (prevState.page !== this.state.page) {
-    //         //console.log("page", this.state.page)
-    //         this.fetchCourses();
-    //     }
-    // }
+    useEffect(() => {
+        const path = window.location.pathname;
+        const parts = path.split('/');
+        const establishmentId = parts[2];
 
-    // setSelectedUniversity = (data) => {
-    //     this.setState({ selectedUniversity: data })
-    // }
+        // Stall listings from specific establishment
+        //axios.get(`http://localhost:5001/api/establishments/${establishmentId}/stalls`)
+        axios.get(`${url}/establishments/${establishmentId}/stalls`)
+            .then((response) => {
+                const data = response.data;
+                setStalls(data);
+                console.log(data)
+            })
+            .catch((error) => {
+                console.error(`Error fetching data: ${error}`);
+            });
 
-    // setSelectedDiscipline = (data) => {
-    //     this.setState({ selectedDiscipline: data })
-    // }
+        // list of establishments 
+        //axios.get(`http://localhost:5001/api/establishments/${establishmentId}`)
+        axios.get(`${url}/establishments/${establishmentId}`)
+            .then((response) => {
+                const data = response.data;
+                console.log(data)
+                setEstablishmentName(data.name);
+            })
+            .catch((error) => {
+                console.error(`Error fetching data: ${error}`);
+            });
+    }, []);
 
-    // setSearchQuery = (data) => {
-    //     // console.log("search: ",data.target.value)
-    //     this.setState({ search: data.target.value })
-    // }
+    return (
+        <Container fluid mx="3px" id="stalls" >
+            <ResponsiveAppBar />
 
-    // setPage = (data) => {
-    //     this.setState({ page: data })
-    // }
+            <div className="flex-container align-content-sm-stretch" id="stalls-header" style={Object.assign({}, styles.paperContainer, { width: "100%", margin: 0, boxShadow: '0px 4px 12px rgba(0, 0, 0, 0.1)' })}>
+                <h1 style={{ fontSize: "28px", textAlign: 'center' }}>{establishmentName}</h1>
+                <div className="flex-container align-content-sm-stretch" id="stalls-subheader">
+                    <h4 style={{ fontSize: "18px", paddingTop: '23px', paddingBottom: '29px' }}>Satisfy your cravings now!</h4>
+                </div>
+            </div>
 
-    // setPageCount = (data) => {
-    //     this.setState({ pageCount: data })
-    // }
-
-    // setTotal = (data) => {
-    //     this.setState({ total: data })
-    // }
-
-    // setLoading = (data) => {
-    //     this.setState({ loading: data })
-    // }
-
-    // getUrlQuery = () => {
-    //     let uniFilter = "&university="
-    //     for (let i = 0; i < this.state.selectedUniversity.length; i++) {
-    //         uniFilter += `${this.state.selectedUniversity[i].value}`;
-    //         if (i < this.state.selectedUniversity.length - 1) {
-    //             uniFilter += ','
-    //         }
-    //     }
-
-    //     let disciplineFilter = "&discipline="
-    //     for (let i = 0; i < this.state.selectedDiscipline.length; i++) {
-    //         disciplineFilter += `${this.state.selectedDiscipline[i].value}`;
-    //         if (i < this.state.selectedDiscipline.length - 1) {
-    //             disciplineFilter += ','
-    //         }
-    //     }
-    //     return `https://dev.silvi.io/api/undergraduate-programmes?search=${this.state.search}&page=${this.state.page}${uniFilter}${disciplineFilter}`;
-    // }
-
-    // resetForm = () => {
-    //     this.setState({
-    //         ...this.state,
-    //         selectedUniversity: [],
-    //         selectedDiscipline: [],
-    //         search: "",
-    //         page: 1,
-    //     }, () => {
-    //         this.fetchCourses();
-    //     })
-    // }
-
-    // handleSubmit = () => {
-    //     this.setState({
-    //         ...this.state,
-    //         page: 1,
-    //     }, () => {
-    //         this.fetchCourses();
-    //     })
-    // }
-
-    // fetchMe = () => {
-    //     axios.get(`https://dev.silvi.io/api/me`,
-    //         { withCredentials: true })
-    //         .then(response => {
-    //             // this.setRiasec_1(response.data.profile.interest_1.name)
-    //             // this.setRiasec_2(response.data.profile.interest_2.name)
-    //             // this.setRiasec_3(response.data.profile.interest_3.name)
-
-    //             return response.data
-    //         })
-    //         .catch(error => {
-    //             console.error('Error', error);
-    //         });
-    // }
-
-    // fetchCourses = () => {
-    //     const URL = this.getUrlQuery();
-    //     axios.get(URL, { withCredentials: true })
-    //         .then(response => {
-    //             this.setCourses_(response.data.results)
-    //             this.fetchMe();
-    //             let contentRange = response.headers["content-range"]
-    //             let total = contentRange.split("/")[1]
-    //             let length = response.data.page_size
-    //             const total_page = Math.ceil(total / length)
-    //             this.setPageCount(total_page)
-    //             this.setTotal(contentRange.split("/")[1])
-    //         })
-    //         .catch(error =>
-    //             console.error('Error', error));
-    // }
-
-    render() {
-        return (
-            <Container fluid mx="3px" id="stalls" >
-                <ResponsiveAppBar />
-
-                <div className="flex-container align-content-sm-stretch" id="stalls-header" style={Object.assign({}, styles.paperContainer, { width: "100%", margin: 0, boxShadow: '0px 4px 12px rgba(0, 0, 0, 0.1)' })}>
-                    <h1 style={{ fontSize: "28px", textAlign: 'center' }}>Frontier</h1>
-                    <div className="flex-container align-content-sm-stretch" id="stalls-subheader">
-                        <h4 style={{ fontSize: "18px", paddingTop: '23px', paddingBottom: '29px' }}>Satisfy your cravings now!</h4>
-                    </div>
+            <div className="container">
+                <div className="subtitile">
+                    <p style={{ fontSize: "16px", paddingTop: "1em", paddingBottom: '2em', textAlign: 'left', paddingLeft: '9em' }}>Here are the list of stalls available in {establishmentName}!</p>
                 </div>
 
-
-                {/* <Row>
-                            <Col className="col-12 col-md-3">
-                                <UniversityFilter
-                                    setSelectedUniversity={this.setSelectedUniversity}
-                                    selectedUniversity={this.state.selectedUniversity}
-                                /></Col>
-                            <Col className="col-12 col-md-3">
-                                <DisciplineFilter
-                                    setSelectedDiscipline={this.setSelectedDiscipline}
-                                    selectedDiscipline={this.state.selectedDiscipline}
-                                /></Col>
-                            <Col className="col-12 col-md-3">
-                                <SearchProgrammes
-                                    setSearchQuery={this.setSearchQuery}
-                                    search={this.state.search}
-                                /></Col>
-                            <Col className="col-12 col-md-3" style={{ padding: "20px", marginBottom: "1em" }}>
-                                <Button className="submit-filter position-absolute bottom-0 end-5 " variant="contained" style={{ marginLeft: "10px", marginRight: "10px", fontSize: "13px" }}
-                                    onClick={() => { this.handleSubmit() }}>
-                                    Submit
-                                </Button>
-                                <Button className="submit-filter position-absolute bottom-0 end-0" size="lg" variant="outlined" style={{ marginLeft: "10px", marginRight: "40px", fontSize: "13px" }}
-                                    onClick={this.resetForm}>
-                                    Reset
-                                </Button>
-                            </Col>
-                        </Row> */}
-
-
-
-                <div className="container">
-                    <div className="subtitile">
-                        <p style={{ fontSize: "16px", paddingTop: "1em", paddingBottom:'2em', textAlign: 'left', paddingLeft: '9em' }}>Here are the list of stalls available in Frontier!</p>
-                    </div>
-
-                    <StallListings />
-
-                    {/* <ProgrammeListings
-                        selectedUniversity={this.state.selectedUniversity}
-                        selectedDiscipline={this.state.selectedDiscipline}
-                        search={this.state.search}
-                        courses_={this.state.courses_}
-                        setCourses_={this.setCourses_}
-                        page={this.state.page}
-                        setPage={this.setPage}
-                        pageCount={this.state.pageCount}
-                    /> */}
-                </div>
-            </Container>
-        )
-    }
+                <StallListings stalls={stalls} />
+            </div>
+        </Container>
+    )
 }
 
 export default Stalls;
